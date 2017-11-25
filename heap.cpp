fc8813ddfe0f18f49f23e40327f06db5da70c711
Ho Lee <clarklee777@Hos-MacBook-Pro.local>
2017-11-25 16:10:06 -0600

    Complete testing heap structure header file

--- heap.cpp
+++ heap.cpp
@@ -1,28 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "heap.hpp"
 // Heap functions are :
 // heap(), max_weight(), max_vertex, delete_max(), Insert(vertex, weight)
 // Delete(vertex), heapify(vertex_bug), heap_size(), change_size(heap_size)
-int main(int argc, char * argv[])
+int main(/*int argc, char *argv[]*/)
 {
     heap * test_heap;
     FILE *fp;
-    fp = fopen("argv[1]", "r");
-    
+    //fp = fopen("argv[1]", "r");
+    fp = fopen("testgraph.txt", "r");
     test_heap = new heap();
     
     if(!fp)
     {
-        if(argv[2]) printf("Error 01 : file: %s not found\n",argv[2]);
+        //if(argv[2]) printf("Error 01 : file: %s not found\n",argv[2]);
         printf("Error 02 : file open failed.\n");
+        //printf("file: %s not found\n",argv[1]);
         printf("proper usage: ./program_name input_filename\nPlease try again...\n\n");
         exit(0);
     }
     else
     {
         int flag = 0;
-        while(flag !=0)
+        while(flag ==0)
         {
             int v1, v2, w, edge;
             fscanf(fp,"%d %d %d", &v1, &v2, &w);
@@ -33,10 +32,12 @@ int main(int argc, char * argv[])
             }
             else
             {
+                
                 /* Make the edge name XX_YY, XX is the small vertex number while YY is larger */
-                if(v1<v2) edge = v1*10 + v2;
-                else edge = v2*10 + v1;
+                if(v1<v2) edge = v1*100 + v2;
+                else edge = v2*100 + v1;
                 test_heap->Insert(edge, w);
+                
             }
             
         }
